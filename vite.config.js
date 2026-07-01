a2dcce83b48a128cb417ae1fb2b86b27f63fc49c
antonifarinango <farinangoanthony1@gmail.com>
2026-07-01 16:11:08 -0500

    produccion-v2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window',  // Esto soluciona el error "global is not defined"
-  },
 })
